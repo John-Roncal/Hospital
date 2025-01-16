7f3954771d3c6f4c5894c5bde429c823117383cc
John-Roncal <johnroncal.19.jarc@gmail.com>
2025-01-16 15:04:44 -0500

    Creación de los métodos CRUD de la entidad Medico en la capa de acceso a datos.

---
+++ Datos/DAL/MedicoDAL.cs
@@ -0,0 +1,43 @@
+﻿using Comun.ViewModels;
+using Modelo.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.DAL
+{
+    public class MedicoDAL
+    {
+        public static ListadoPaginadoVMR<MedicoVMR> LeerTodo(int cantidad, int pagina, string textoBusqueda)
+        {
+            ListadoPaginadoVMR<MedicoVMR> resultado = new ListadoPaginadoVMR<MedicoVMR>();
+            return resultado;
+        }
+
+        public static MedicoVMR LeerUno(long id)
+        {
+            MedicoVMR item = null;
+
+            return item;
+        }
+
+        public static long Crear(Medico item)
+        {
+            long id = 0;
+
+            return id;
+        }
+
+        public static void Actualizar(MedicoVMR item)
+        {
+            
+        }
+
+        public static void Eliminar(List<long> ids)
+        {
+
+        }
+    }
+}
