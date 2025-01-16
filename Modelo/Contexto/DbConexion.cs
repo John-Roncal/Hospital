4e4d4bd7a978357abf831c877034cc2817563e39
John-Roncal <johnroncal.19.jarc@gmail.com>
2025-01-16 09:54:09 -0500

    Conexion con la base de datos a través de Entity Framework y creación de un extensión de la clase DbConext para modificar configuraciones como el LazyLoadingEnable = false y ProxyCreationEnable = false para evitar futuros errores al momento de usar las entidades en json y no haya una referencia en bucle entre ellas. Además de un método para la creación de conexion.

---
+++ Modelo/Contexto/DbConexion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Modelos
+{
+    public partial class DBConexion : DbContext
+    {
+        private DBConexion(string stringConexion) 
+            :  base(stringConexion)
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+            this.Database.CommandTimeout = 900;
+        }
+
+        public static DBConexion Create()
+        {
+            return new DBConexion("name=DBConexion");
+        }
+    }
+}
