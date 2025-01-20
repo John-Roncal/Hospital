c933828362bb2ae4004b0ab469eb453103682c75
John-Roncal <johnroncal.19.jarc@gmail.com>
2025-01-20 16:10:54 -0500

    Creación de las clases para las entidades en la Capa Lógica y de los métodos para la clase MedicoBLL. Además, se creo el controller para la entidad médico.

--- Datos/DAL/MedicoDAL.cs
+++ Datos/DAL/MedicoDAL.cs
@@ -13,12 +13,51 @@ namespace Datos.DAL
         public static ListadoPaginadoVMR<MedicoVMR> LeerTodo(int cantidad, int pagina, string textoBusqueda)
         {
             ListadoPaginadoVMR<MedicoVMR> resultado = new ListadoPaginadoVMR<MedicoVMR>();
+
+            using (var db = DBConexion.Create())
+            {
+                var query = db.Medico.Where(x => !x.borrado).Select(x => new MedicoVMR
+                {
+                    id = x.id,
+                    cedula = x.cedula,
+                    nombre = x.nombre + " " + x.apellidoPaterno + (x.apellidoMaterno != null ? (" " + x.apellidoMaterno) : ""),
+                    esEspecialista = x.esEspecialista,
+                });
+
+                if (!string.IsNullOrEmpty(textoBusqueda))
+                {
+                    query = query.Where(x => x.cedula.Contains(textoBusqueda) || x.nombre.Contains(textoBusqueda));
+                }
+
+                resultado.cantidadTotal = query.Count();
+
+                resultado.elemento = query
+                    .OrderBy(x => x.id)
+                    .Skip(pagina = cantidad)
+                    .Take(cantidad)
+                    .ToList();
+            }
+
             return resultado;
         }
 
         public static MedicoVMR LeerUno(long id)
         {
             MedicoVMR item = null;
+
+            using (var db = DBConexion.Create())
+            {
+                item = db.Medico.Where(x => !x.borrado && x.id == id).Select(x => new MedicoVMR
+                {
+                    id = x.id,
+                    cedula = x.cedula,
+                    nombre = x.nombre,
+                    apellidoPaterno = x.apellidoPaterno,
+                    apellidoMaterno = x.apellidoMaterno,
+                    habilitado = x.habilitado,
+                    esEspecialista = x.esEspecialista
+                }).FirstOrDefault();
+            }
 
             return item;
         }
@@ -27,17 +66,48 @@ namespace Datos.DAL
         {
             long id = 0;
 
+            using (var db = DBConexion.Create())
+            {
+                item.borrado = false;
+                db.Medico.Add(item);
+                db.SaveChanges();
+            }
+
             return id;
         }
 
         public static void Actualizar(MedicoVMR item)
         {
-            
+            using (var db = DBConexion.Create())
+            {
+                var itemUpdate = db.Medico.Find(item.id);
+
+                itemUpdate.cedula = item.cedula;
+                itemUpdate.nombre = item.nombre;
+                itemUpdate.apellidoPaterno = item.apellidoPaterno;
+                itemUpdate.apellidoMaterno = item.apellidoMaterno;
+                itemUpdate.habilitado = item.habilitado;
+                itemUpdate.esEspecialista = item.esEspecialista;
+
+                db.Entry(itemUpdate).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
         }
 
         public static void Eliminar(List<long> ids)
         {
+            using (var db = DBConexion.Create())
+            {
+                var items = db.Medico.Where(x => ids.Contains(x.id));
 
+                foreach (var item in items)
+                {
+                    item.borrado = true;
+                    db.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                }
+
+                db.SaveChanges();
+            }
         }
     }
 }
